e374fc01a1b5c983faf751bc419b0441e783e8e8
RonaldBorda <ronald.bordab@gmail.com>
2026-05-08 03:09:41 -0500

    subida al repo

---
+++ backend/src/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const cors = require('cors');
+const testRoutes = require('./routes/test.routes');
+const app = express();
+
+app.use(cors());
+app.use(express.json()); 
+app.use('/api', testRoutes);
+
+module.exports = app;    
---
+++ backend/src/config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+
+        console.log('MongoDB conectado');
+    } catch (error) {
+        console.log('Error de conexión:', error.message);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
